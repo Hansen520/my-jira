7216d9fef4b7520088d79ea0a7022376779ecdba
Hansen1994 <646380243@qq.com>
2022-04-10 20:06:37 +0800

    表单TS提交

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { ProjectListScreen } from './screens/project-list/index'
-import { LoginScreen } from './screens/login/login'
+import { useAuth } from 'screens/context/auth-context'
+import { AuthenticatedApp } from "authenticated-app";
+import { UnauthenticatedApp } from 'unauthenticated-app/index'
 import './App.css';
 
 function App() {
+  const { user } = useAuth()
   return (
     <div className="App">
-      <LoginScreen/>
+      { user ? <AuthenticatedApp/> : <UnauthenticatedApp/>}
     </div>
   );
 }
--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,11 +1,10 @@
-import { useEffect } from 'react'
-
 export interface User {
   id: string
   name: string
   email: string
   title: string
   organization: string
+  token: string
 }
 interface SearchPanelProps {
   users: User[]
@@ -16,25 +15,32 @@ interface SearchPanelProps {
   setParam: (param: SearchPanelProps["param"]) => void// 里面就是说param参数是{name: stringpersonId: string}类型的，然后返回空
 }
 export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
-  return <form>
-    <input type="text" value={param.name} onChange={evt => setParam({
-      ...param,
-      name: evt.target.value
-    })} />
+  return (<form>
+    <div>
+      <input
+        type="text"
+        value={param.name}
+        onChange={
+          evt => setParam({
+            ...param,
+            name: evt.target.value
+          })
+        } />
     
-    <select value={param.personId} onChange={evt => setParam({
-      ...param,
-      name: evt.target.value
-    })}>
-      <option value={''}>负责人</option>
-      {users.map(user => <option key={user.id} value={user.name}>{ user.name }</option>) }
-    </select>
-  </form>
+      <select
+        value={param.personId}
+        onChange={
+          evt => setParam({
+            ...param,
+            name: evt.target.value
+          })
+        }>
+        <option value={''}>负责人</option>
+        {users.map(user =>
+          <option key={user.id} value={user.name}>
+            {user.name}
+          </option>)}
+      </select>
+    </div>
+  </form>)
 }
-
-// // 初始化
-// export const useMount = (callback) => {
-//   useEffect(() => {
-//     callback()
-//   }, [])
-// }
